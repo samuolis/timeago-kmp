453ce89da5937436893a9e1823a54ee3654288e6
lukas.samuolis <lukas.samuolis@vinted.com>
2026-01-05 16:37:27 +0200

    Add group

--- timeago-kmp/build.gradle.kts
+++ timeago-kmp/build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
     alias(libs.plugins.mavenPublish)
 }
 
+group = "io.github.samuolis"
+
 kotlin {
     // Explicit API mode - forces visibility modifiers and return types
     explicitApi()
